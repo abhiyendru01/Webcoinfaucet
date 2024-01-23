c9ead6b7e5509e3473b88be964908aca54ce8293
Rahul Raj <118946861+abhiyendru01@users.noreply.github.com>
2024-01-24 04:25:19 +0530

    Co-authored-by: Aman kumar <Amanlabh@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,220 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+
+  useEffect(() => {
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* MetaMask is installed */
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWalletAddress("");
+      console.log("Please install MetaMask");
+    }
+  };
+  const boxClick = (boxNumber) => {
+    alert(`Box ${boxNumber} clicked!`);
+    
+  };
+  return (
+    
+    <div>
+      
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size-4">Webcoin Token(WCT)</h1>
+          </div>
+          
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-end is-align-items-center">
+              <button
+                className="button-40"
+                onClick={connectWallet}
+              >
+                <span className="is-link has-text-weight-bold">
+                  {walletAddress && walletAddress.length > 0
+                    ? `Connected: ${walletAddress.substring(
+                        0,
+                        6
+                      )}...${walletAddress.substring(38)}`
+                    : "Connect Wallet"}
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section className="hero is-fullheight">
+        <div className="faucet-hero-body">
+          <div className="container has-text-centered main-content">
+            <h1 className="title is-1">Webcoin Token</h1>
+            <p>Fast and reliable. 50 OCT/day.</p>
+            <div className="box address-box">
+              <div className="columns">
+                <div className="column is-four-fifths">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    placeholder="Enter your wallet address (0x...)"
+                  />
+                </div>
+                <div className="column">
+                  <button className="button-40">
+                    GET TOKENS
+                  </button>
+                </div>
+              </div>
+              <article className="panel is-grey-darker">
+                <p className="panel-heading">Transaction Data</p>
+                <div className="panel-block">
+                  <p>transaction data</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <div className="containerr">
+        <div className="boxx" >
+          <a href="https://discord.com/api/oauth2/authorize?client_id=1199456588096864378&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A1500%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=identify+guilds+gdm.join+email+guilds.join+connections">
+          <image src="public/discord.png" alt="" />
+          <p>Discord</p>
+          </a>
+        </div>
+  
+        <div className="boxx" onClick={() => boxClick(2)}>
+          <img src="src/images/twitter.svg" alt="" />
+          <p>Twitter</p>
+        </div>
+  
+        <div className="boxx" onClick={() => boxClick(3)}>
+          <a href="#">
+            <img src="src/images/github.svg" alt="" />
+            <p>Github</p>
+          </a>
+          
+        </div>
+      </div>
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+        <div className="w-full flex flex-col items-center justify-center m-auto">
+            <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+                
+
+                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                    <div className="font-bold text-[16px]">Community</div>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        <a href="#"></a>
+                        
+                        <span className="text-[15px] ml-[6px]" >Youtube</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        
+                        <span className="text-[15px] ml-[6px]">Github</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        
+                        <span className="text-[15px] ml-[6px]">Discord</span>    
+                    </p>
+                </div>
+                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                    <div className="font-bold text-[16px]">Social Media</div>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        
+                        <a href="https://instagram.com/abhiyendru">
+                        <span className="text-[15px] ml-[6px]">Instagram</span> </a>   
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        
+                        <span className="text-[15px] ml-[6px]">Twitter</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        
+                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
+                    </p>
+                </div>
+                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                    <div className="font-bold text-[16px]">About</div>
+                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                     
+                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                      
+                        <span className="text-[15px] ml-[6px]">Learn about us</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                  
+                        <span className="text-[15px] ml-[6px]">webcointkn@gmail.com</span>    
+                    </p>
+                </div>
+            </div>
+            <div className="mb-[20px] text-[15px] text-center">
+                
+                
+            </div>
+
+            <div className="mb-[20px] text-[15px] text-center">
+                &copy; Webcoin Token 2024 Inc. All rights reserved
+                
+            </div>
+            <div className="mb-[20px] text-[15px] text-center">
+                Designed By Clueless Coders
+                
+            </div>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
